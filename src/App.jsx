e4b5d9cc995b1b5e241a77e6965731028f7d66e9
CosmicRick <supradiproy737@gmail.com>
2026-01-13 00:54:40 +0530

    end time

--- src/App.jsx
+++ src/App.jsx
@@ -754,7 +754,7 @@ const App = () => {
                 <div className="input-row">
                   <FontAwesomeIcon icon={faCalendarCheck} /><span>End Event:</span>
                   <input className="endDate" name="endDate" type="date" />
-                  <span>End Time:</span>
+                  <span>EndTime:</span>
                   <input className="endTime" name="endTime" type="time" />
                 </div>
                 <div className="input-row">
